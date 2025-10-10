8f66b6131219256a095b8738df47607966c7fec7
guiantoniazzi <guilherme.antoniazzi@outlook.com>
2025-10-10 19:03:05 -0300

    feat: env vars

--- src/repository/connection.ts
+++ src/repository/connection.ts
@@ -2,12 +2,16 @@ import mysql from 'mysql2/promise';
 import { Sequelize } from 'sequelize';
 
 var con = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DB_GEST_MAIS'
+    host: process.env.HOST || 'localhost',
+    user: process.env.HOST_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'DB_GEST_MAIS',
 });
 
-const conSequelize = new Sequelize('DB_GEST_MAIS', 'root', '', {dialect: 'mysql', host: 'localhost'});
+const conSequelize = new Sequelize(
+    process.env.DB_NAME || 'DB_GEST_MAIS',
+    process.env.HOST_USER || 'root',
+    process.env.DB_PASSWORD || '', 
+    {dialect: 'mysql', host: process.env.HOST || 'localhost'});
 
 export { con, conSequelize };
